f843f02be797c24f05579a49683079885bc46a33
Aditya Kavale <adityakavale21@gmail.com>
2021-04-18 19:07:59 +0530

    Update datatransfer.js

--- Login/datatransfer.js
+++ Login/datatransfer.js
@@ -1,5 +1,5 @@
 function login(){
     var b = document.getElementById('name').value
     document.getElementById('here').innerHTML = "welcome " + b;
-    window.location.href = "Welcome_Page.html";
+    window.location.href = "https://adu2120.github.io/Online_Exam_System/Welcome_Page.html";
 }
