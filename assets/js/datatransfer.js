7f973aff9eb883143ae0230d84c428deb3dedade
Aditya Kavale <adityakavale21@gmail.com>
2021-04-18 18:56:22 +0530

    u

--- assets/js/datatransfer.js
+++ assets/js/datatransfer.js
@@ -1,4 +1,5 @@
 function login(){
     var b = document.getElementById('name').value
     document.getElementById('here').innerHTML = "welcome " + b;
+    window.location.href = "Welcome_Page.html";
 }
